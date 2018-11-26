cba1a878f4a106d0862c7a972152b5cd400aa4eb
rodriguez27298 <rodriguez27298@users.noreply.github.com>
2018-11-26 11:32:26 -0800

    Exercise 9

--- mainFunction.js
+++ mainFunction.js
@@ -8,15 +8,33 @@ if (theme == null){
 	if (theme == 'light') {
 		fg_color = 'black';
 		bg_color = 'white';
+		link_color = 'black';
 	}
 	else if (theme == 'dark') {
 		fg_color = 'white';
 		bg_color = 'black';
-}
+		link_color = 'white';
+	}
+	else if (theme == 'blue') {
+		fg_color = 'white';
+		bg_color = 'blue';
+		link_color = 'blue';
+	}
+	else if (theme == 'red') {
+		fg_color = 'white';
+		bg_color = 'red';
+		link_color = 'red';
+	}	
 	document.body.style.color = fg_color;
 	document.body.style.backgroundColor = bg_color;
+	document.getElementById("link").style.color = link_color;
 	localStorage.setItem('theme', theme);
 
 }
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
 var saved_theme = localStorage.getItem('theme');
 my_theme(saved_theme);
